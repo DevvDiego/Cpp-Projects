e835a36afb36e6d359e3a079533450f2c15c88d7
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-10-18 20:55:05 -0600

    Added auto resize to Entrada

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ int main(){
 
     Parking parking("ola", 10);
     std::cout<<parking.getNombre()<<std::endl;
+    std::cout<<parking.getMaxSpots()<<std::endl;
 
+
+
+    std::cout<<"\n\n";
+    parking.Entrada("123-MXSD", 10);
+    std::cout<<parking.getMaxSpots()<<std::endl;
+
+    parking.Entrada("12321312312", 15);
+    std::cout<<parking.getMaxSpots()<<std::endl;
+
+    parking.Entrada("12321312312", 20);
+    std::cout<<parking.getMaxSpots()<<std::endl;
 
     return 0;
 }
--- parking.cpp
+++ parking.cpp
@@ -6,35 +6,64 @@ class Parking{
     private:
     std::string name;
     std::vector<std::string> plates;
+    size_t totalSpots;
 
     public:
 
                                             //inicializando metodos
-    Parking(std::string name, int tamaño) : plates(tamaño,"") {
+    Parking(std::string name, int size) : plates(size,"") {
         this->name = name; 
-
+        this->totalSpots = this->plates.capacity();
     }
 
-    std::string getNombre(){
-        return this->name;
-
-    }
-
-    void Entrada(std::string plate, int plaza){
+    void Entrada(std::string plate, int spot){
         
         if( plate.length() < 4 || plate == "" ){
-            throw "plate incorrecta";
+
+            throw "Matricula incorrecta";
         }
 
-        if( !this->plates.at(plaza).empty() ){
+        if( spot >= this->plates.size() ){
+
+            while( spot >= this->plates.size() ){
+                /**
+                 * Sets the new size of the vector to 
+                 * the same size it had, but adds one due to
+                 * it checking always that it has >= of the max size
+                 * 
+                 * (max size plus one)
+                 */
+                this->plates.resize(this->plates.size() + 1, "");
+                
+                this->totalSpots = this->plates.size();
+            }
+
+        }
+
+        if( !this->plates.at(spot).empty() ){
+
             throw "Plaza ya ocupada";
         }
 
         if( itExists(plate) ){
+
             throw "Matricula repetida";
         }
 
+        this->plates[spot];
+
     }
+
+    std::string getNombre(){
+
+        return this->name;
+    }
+
+    size_t getMaxSpots(){
+
+        return this->totalSpots;
+    }
+
 
     private: 
     bool itExists(std::string item){
