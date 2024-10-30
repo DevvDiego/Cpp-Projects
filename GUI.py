719783b47c8a0b68054cf87b7d8f4f0556b3f53a
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-10-29 22:06:24 -0600

    Make basic connection between py and cpp

---
+++ GUI.py
@@ -0,0 +1,65 @@
+import subprocess,os
+
+def send_cpp(msg):
+    # Send the user input to the C++ program's stdin
+    process.stdin.write(str(msg) + "\n")  # "\n marks end of write"
+    process.stdin.flush()  # Flush the stream to ensure the input is sent immediately
+
+def read_cpp():
+    output = process.stdout.readline().strip()
+    return output.split(',');
+
+
+
+
+
+process = subprocess.Popen(
+    ['./app.exe'],  # List containing the path to the C++ executable
+    stdin=subprocess.PIPE,  # Connect the subprocess's standard input (stdin) to a pipe
+    stdout=subprocess.PIPE,  # Connect the subprocess's standard output (stdout) to a pipe
+    text=True  # Treat the stdin/stdout as text streams (instead of binary)
+)
+
+while True:
+    
+    print(
+        "Elige una opcion\n"+
+        "1) Entrada de coche\n"
+        "2) Salida de coche\n"
+        "3) Mostrar todo\n"
+        "4) Salir del programa\n"
+        );
+
+    user_input = str(input("PYTHON Enter a command: "));
+    
+    if user_input.lower() == 'exit':
+
+        send_cpp(user_input);
+        print("CPP exit code: ", read_cpp());
+        break
+
+    if(user_input == "1"):
+        send_cpp(user_input);
+
+        send_cpp( input("Matricula: ") );
+
+        send_cpp( input("Plaza: ") );
+
+        print(read_cpp())
+        print(read_cpp())
+        print(read_cpp())
+        
+    elif(user_input == "3"):
+
+        send_cpp(user_input);
+        
+        for item in read_cpp():
+            print(item + "\n");
+
+
+
+process.terminate(); # Terminate the subprocess 
+process.wait(); # Wait for the subprocess to exit
+
+
+os.system("pause");
--- main.cpp
+++ main.cpp
@@ -37,15 +37,17 @@ int main(){
 
         try{
 
-            std::cout<<"\n\n\n-------------------\n";
-            std::cout<<"Parking centro\n";
-            std::cout<<"Elige una opcion\n"<<
-            "1) Entrada de coche\n2) Salida de coche\n3) Mostrar todo\n4) Salir del programa\n";
+            // std::cout<<"\n\n\n-------------------\n";
+            // std::cout<<"Parking centro\n";
+            // std::cout<<"Elige una opcion\n"<<
+            // "1) Entrada de coche\n2) Salida de coche\n3) Mostrar todo\n4) Salir del programa\n";
             
             // \b se usa para regresar un caracter anterior en la terminal
-            std::cout<<"-------(   )-------\b\b\b\b\b\b\b\b\b\b";
+            // std::cout<<"-------(   )-------\b\b\b\b\b\b\b\b\b\b";
+            
             input(opcion);
-            std::cout<<"\n";
+
+            // std::cout<<"\n";
 
             switch(opcion){
 
@@ -53,18 +55,15 @@ int main(){
                     
                 try{
 
-                    std::cout<<"Ingresa la matricula: ";
+                    // std::cout<<"Ingresa la matricula: ";
                     input(matricula);
-                    // std::cin>>matricula;
 
-                    std::cout<<"Ingresa la plaza: ";
+                    // std::cout<<"Ingresa la plaza: ";
                     input(plaza);
-                    // std::cin>>plaza;
 
                     parking.Entrada(matricula, plaza);
 
-                    std::cout<<"\n----Ingreso exitoso----\n\n";
-
+                    // std::cout<<"\n----Ingreso exitoso----\n\n";
                     std::cout<<parking.getTotalSpots()<<" Plazas totales\n";
                     std::cout<<parking.getOccupiedSpots()<<" Plazas ocupadas\n";
                     std::cout<<parking.getFreeSpots()<<" Plazas disponibles"<<std::endl;
@@ -96,7 +95,8 @@ int main(){
                 
                 case 3: //mostrar todo
 
-                    std::cout<<parking.toString();
+                    // std::cout<<"opc 3"<<std::endl;
+                    std::cout<<parking.toString()<<std::endl;
                 break;
                 
                 case 4: //salir del programa
@@ -114,23 +114,6 @@ int main(){
             std::cerr<<"Error desconocido";
         }
     }
-
-
-
-
-
-
-    // parking.Entrada("000-MEX", 0);
-    // parking.Entrada("123-MEX", 1);
-    // parking.Entrada("321-MEX", 2);
-    // parking.Entrada("456-MEX", 3);
-    // parking.Entrada("654-MEX", 4);
-    // parking.Entrada("789-MEX", 5);
-    // parking.Entrada("987-MEX", 6);
-    // parking.Entrada("136-MEX", 7);
-    // parking.Entrada("631-MEX", 8);
-    // parking.Entrada("247-MEX", 9);
-    // parking.Entrada("742-MEX", 10);
 
     return 0;
 }
