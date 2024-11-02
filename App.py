6588f690b648b84c3e5de24d3d99af56cb06c4c6
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-11-02 17:30:02 -0600

    End Insertar pagina

--- App.py
+++ App.py
@@ -115,7 +115,7 @@ class Pagina_Insertar:
 
     def __init__(self, frameContainer):
         self.frame = tk.Frame(frameContainer)
-        
+
         self.createWidgets()
         self.configWidgets()
         self.placeWidgets()
@@ -126,26 +126,51 @@ class Pagina_Insertar:
             height=2, bg="gray13",fg="white",
         )
 
-
         self.inputPlateLabel.config(text="Ingresa la matricula", font=("Roboto", 12))
-        self.inputPlate.config(textvariable=self.matricula)
+        self.inputPlate.config(textvariable=self.plate)
+
+        self.inputSpotLabel.config(text="Ingresa el lugar", font=("Roboto", 12))
+        self.inputSpot.config(textvariable=self.spot)
+
+        self.spacer.config(text="", pady=20)
+
+        self.btnEnviar.config(
+            text="Aceptar", font=("Verdana", 10),
+            padx=5, pady=5, command=lambda: print("Plate: " + self.inputPlate.get() + " Spot: " + self.spot.get())
+        )
 
     def placeWidgets(self):
         self.title.pack(fill="x")
-        self.inputPlateLabel.pack()
+        
+        self.inputPlateLabel.pack(pady=(40,0))
         self.inputPlate.pack()
+
+        self.inputSpotLabel.pack(pady=(40,0))
+        self.inputSpot.pack()
+
+        self.spacer.pack()
+
+        self.btnEnviar.pack()
+
        
 
     def createWidgets(self):
         self.title = tk.Label(self.frame)
 
-        self.matricula = tk.StringVar()
+        self.plate = tk.StringVar()
         self.inputPlateLabel = tk.Label(self.frame)
-        self.inputPlate = tk.Entry(self.frame) 
+        self.inputPlate = tk.Entry(self.frame)
 
-    def getMatricula(self):
-        return self.matricula.get()
-        
+        self.spot = tk.StringVar()
+        self.inputSpotLabel = tk.Label(self.frame)
+        self.inputSpot = tk.Entry(self.frame)
+
+        self.spacer = tk.Label(self.frame)
+
+        self.btnEnviar = tk.Button(self.frame)
+
+        # self.btn = tk.Button(self.frame, text="Mandar", command=lambda: print(self.matricula.get()))
+
     def getFrame(self):
         return self.frame
 
