c780124c5452c994db3b4d412b7e2087d8c890a4
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-11-06 19:11:05 -0600

    Make cpp subprocess exit when tkinter gui has been closed

--- App.py
+++ App.py
@@ -4,3 +4,8 @@ from CppHandler import CppHandler
 cpp = CppHandler()
 
 gui(cppHandler=cpp)
+
+
+# After the tkinter gui has been killed, the subprocess might still be
+# running in the background
+cpp.endProgram() # Salida del subproceso
