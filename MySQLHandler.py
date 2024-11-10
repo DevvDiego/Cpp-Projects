0dfb7a618ed88b884d7b08e24466fb0bc6cee124
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2024-11-09 18:22:41 -0600

    Revert to basic mysql calls

--- MySQLHandler.py
+++ MySQLHandler.py
@@ -7,77 +7,47 @@ class MySqlHandler:
 
     def __init__(self, credentials:dict):
         self.credentials = credentials
-    
 
-    def dbexecute(self, query:str, values:tuple=("",)) -> list[tuple] | None:
-        #have the values tuple to a default tuple in case there is a read op
-        # so interpreter doesnt mark an error
+    def insert(self, values:tuple):
         conn = mysql.connect(**self.credentials); #discompose the dict
         cursor = conn.cursor();
-        
-        if(MySqlHandler.isRead):
-            cursor.execute(query);
-            result = cursor.fetchall()
 
-            cursor.close();
-            conn.close();
-        
-            return result;
-    
-        cursor.execute(query, values);
+        cursor.execute(
+            operation="INSERT INTO matriculas (matricula, plaza) VALUES (%s, %s)",
+            params=values 
+        );
         conn.commit();
+
+        cursor.close();
+        conn.close();    
+
+    def read_all(self) -> list[tuple]:
+        conn = mysql.connect(**self.credentials); #discompose the dict
+        cursor = conn.cursor();
+
+        cursor.execute(
+            operation="SELECT * FROM matriculas", 
+        );
+        result = cursor.fetchall();
 
         cursor.close();
         conn.close();
 
-
-
-    #? might be useful to make cpp and db go in the same data path?
-    # def dbexecutemany(self, query:str, values:tuple):
-    #     conn = mysql.connect(**self.credentials); #discompose the dict
-    #     cursor = conn.cursor();
-        
-    #     cursor.executemany(query, values);
-    #     conn.commit();
-
-    #     cursor.close();
-    #     conn.close();
-
-    def insert(self, values:tuple):
-        query = "INSERT INTO matriculas (matricula, plaza) VALUES (%s, %s)";
-        self.dbexecute(query, values);
-
-        print("MySQL insert action");
-
-    def read(self, val:str):
-        MySqlHandler.isRead = True;
-
-        query = "SELECT * FROM matriculas WHERE matricula =" + val;
-        self.cursor.execute(query)
-        self.db.commit();
-        result = self.cursor.fetchall();
-
-        print("MySQL read action");
-        return result
+        print("MySQL read all action");
+        return result  
     
-    def readAll(self):
-        MySqlHandler.isRead = True;
+    def delete(self, value:tuple):
+        conn = mysql.connect(**self.credentials); #discompose the dict
+        cursor = conn.cursor();
 
-        #? add to dbexecute trouhg a flag param?
-        query = "SELECT * FROM matriculas";
+        cursor.execute(
+            operation="DELETE FROM matriculas WHERE matricula = %s",
+            params=value 
+        );
+        conn.commit()
 
-        result = self.dbexecute(query)
+        cursor.close();
+        conn.close();
+
+        print("MySQL delete action"); 
         
-        print("mysql readAll")
-
-        #mark isRead as false again after operations
-        MySqlHandler.isRead= False;
-        return result;
-
-    def delete(self, values:tuple):
-        query = "DELETE FROM matriculas WHERE matricula = %s";
-        self.dbexecute(query, values);
-
-
-        print("MySQL delete action");
-        return
